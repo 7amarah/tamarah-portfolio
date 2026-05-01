c8b43b2cbbc2a38164142ddd97f9480d9f0c77fc
Tamarah Waritimi <kaemi-c@outlook.com>
2026-05-01 17:27:54 +0100

    create a constant for the navigation links

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const NAV_ITEMS = [
+    { label: "Home", href: "/" },
+    { label: "Projects", href: "/projects" },
+    { label: "About", href: "/about" },
+    { label: "Experience", href: "/experience" },
+    { label: "Contact", href: "/contact" },
+];
