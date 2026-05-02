5d9e12e870c51527e88d51599baff722a2a212ca
Tamarah Waritimi <kaemi-c@outlook.com>
2026-05-02 19:35:06 +0100

    add ariaLabel property to the individual NAV_ITEMS objects

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,23 @@
 export const NAV_ITEMS = [
     { label: "Home", href: "/" },
-    { label: "Projects", href: "/projects" },
-    { label: "About", href: "/about" },
-    { label: "Experience", href: "/experience" },
-    { label: "Contact", href: "/contact" },
+    {
+        label: "Projects",
+        href: "/projects",
+        ariaLabel: "View Tamarah's projects",
+    },
+    {
+        label: "About",
+        href: "/about",
+        ariaLabel: "Learn more about Tamarah",
+    },
+    { 
+        label: "Experience",
+        href: "/experience",
+        ariaLabel: "View Tamarah's experience",
+    },
+    { 
+        label: "Contact", 
+        href: "/contact",
+        ariaLabel: "Get in touch with Tamarah"
+    },
 ];
