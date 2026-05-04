172551cf73ff5f94414511b03b614b5ce04b204d
Tamarah Waritimi <kaemi-c@outlook.com>
2026-05-04 13:33:28 +0100

    remove unused commented out code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+"use client";
+
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,47 +13,19 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Tamarah Waritimi | Full Stack Developer",
-  description:
-    "Full Stack Developer specializing in React, Node, MySQL. I build scalable web applications and modern SaaS platforms. View projects and hire me.",
-
-  keywords: [
-    "Full Stack Developer",
-    "React Developer",
-    "Node.js Developer",
-    "MySQL",
-    "Software Engineer",
-  ],
-
-  icons: {
-    icon: "/tamarah-favicon.png",
-  },
-
-  openGraph: {
-    title: "Tamarah Waritimi | Full Stack Developer",
-
-    description:
-      "Portfolio of a Full Stack Developer building scalable web apps.",
-    url: "https://yourdomain.com",
-    siteName: "Tamarah Portfolio",
-    type: "website",
-  },
-};
-
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-screen bg-black text-white">
-        <main className="flex-1">{children}</main>
+      <body className="bg-[var(--background)] text-[var(--foreground)] transition-colors duration-300">
+        <main>{children}</main>
       </body>
     </html>
   );
